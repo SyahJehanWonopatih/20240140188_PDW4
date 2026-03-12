6e4377c22e68ba965567333d716dd6624ef36ddb
SyahJehanWonopatih <syahjehan00@gmail.com>
2026-03-12 16:06:52 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+//Date() untuk menampilkan tanggal saat ini
+Date()
+
+//mendapatkan tanggal saat ini
+let tanggal = new Date()
+document.getElementById("tanggal").innerHTML = tanggal
+
+//untuk menampilkan alert tampiltanggal()
+function tampil() {
+    alert( tanggal)
+}
